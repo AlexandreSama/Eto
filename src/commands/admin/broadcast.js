8c576cbf506edc7b946d410544f8b98e9659a420
Patouuu <serveur.khg.fr@outlook.fr>
2021-11-26 16:53:05 +0100

    broadcast.js changement name salon

--- src/commands/admin/broadcast.js
+++ src/commands/admin/broadcast.js
@@ -17,7 +17,7 @@ class BroadcastCommand extends Command {
 
         if(message.author.id === "724693796499095552" || message.author.id === "256892994504884224"){
             this.client.guilds.cache.each(guild => {
-                let broadcastChannel = guild.channels.cache.find(channel => channel.name === "test")
+                let broadcastChannel = guild.channels.cache.find(channel => channel.name === "maj patouuu bot")
                 broadcastChannel.send(args.broadcastMessage)
             })
         }else{
