1948de374126748e0da7baef72ceecbf06f8a5bf
Patouuu <serveur.khg.fr@outlook.fr>
2021-11-28 18:06:46 +0100

    Ajouts images + modif embed

--- src/commands/misc/lienstaff.js
+++ src/commands/misc/lienstaff.js
@@ -15,10 +15,11 @@ class LienStaffCommand extends Command {
          message.delete()
 
         let lien =  new Discord.MessageEmbed()
-        .setColor('#00FFB2')
+        .setColor('F000FF')
         .setTitle(`Liens utiles Staff`)
         .setAuthor(message.author.username, message.author.displayAvatarURL({dynamic : true})) 
         .setDescription('Suivis point staffing [ici](https://docs.google.com/spreadsheets/d/1U3pqUOU3Q6KPn2gTeEzMZTmfFl2_iwoZgWoEvkU8448/edit?usp=sharing).\n Lien demande de ban [ici](https://docs.google.com/spreadsheets/d/18nubdvEoslLaVFnhqPpBKZxH2OaboVkuS5aAzL-O_bk/edit?usp=sharing). \n Lien tableau des sanctions [ici](https://docs.google.com/spreadsheets/d/1BspOHvDxtI4i1z3KWaeOJeHl_wYM2GGBxbiM9qhOqRI/edit?usp=sharing). \n Lien report bug (serveur) [ici](https://docs.google.com/spreadsheets/d/1paFfcFIEqWnLNzd0_oRpEnNNGMUT0wecLYDG6tmLce8/edit?usp=sharing).')
+        .setImage('https://cdn.discordapp.com/attachments/870356020906577970/914556696997822494/liens_staff_2.gif')
         .setTimestamp() 
         .setFooter(`Commande by Phénix Team's | Tout droit reservée © 2020-2022`)
         
