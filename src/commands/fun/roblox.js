48b3423303673941f1575177c7b8b27bcfe97b05
Alexλndre <56075082+AlexandreSama@users.noreply.github.com>
2021-11-29 20:54:30 +0100

    fix roblox cmd error

--- src/commands/fun/roblox.js
+++ src/commands/fun/roblox.js
@@ -7,8 +7,8 @@ const moment = require('moment')
 
 class RobloxCommand extends Command {
     constructor() {
-        super('punch', {
-            aliases: ['punch'],
+        super('roblox', {
+            aliases: ['roblox'],
             args: [{
                 id: "robloxUsername", match: "content"
             }]
@@ -25,6 +25,8 @@ class RobloxCommand extends Command {
 
         const data = result.body.Id; 
 
+        console.log('test1')
+
         if (args.robloxUsername.length < 1) return message.channel.send("Tu a besoin de me donner un pseudo pour cet commande")
 
         if (result.body.Id === "undefined") return message.channel.send("Impossible de trouver le compte a qui appartient ce pseudo " + saybot)
@@ -32,23 +34,22 @@ class RobloxCommand extends Command {
         const url2 = `https://api.roblox.com/ownership/hasasset?userId=${data}&assetId=102611803`; 
             
         snekfetch.get(url2).then(a => { 
-                const Verifiedcheck = a.body 
                 let lien =  new Discord.MessageEmbed() 
                     .setColor(0x00A2E8) 
                     .setTitle("Pseudonyme: " + args.robloxUsername) 
-                    .setDescription("User ID: " + data) 
-                    .addField("Compte Vérifié ?", Verifiedcheck) 
+                    .setDescription("User ID: " + data)
                     .setFooter("Lien vers le profile : " + `https://web.roblox.com/users/${data}/profile`) 
                     .setThumbnail("https://roblox.com/Thumbs/BCOverlay.ashx?username=" + args.robloxUsername) 
                     .setImage("http://www.roblox.com/Thumbs/Avatar.ashx?x=100&y=100&Format=Png&username=" + args.robloxUsername); 
                 const url3 = `https://users.roblox.com/v1/users/${data}`
+                console.log('test2')
 
                 snekfetch.get(url3).then(b => {
                     let result = b.body
                     let date = moment(result.created).format("YYYY-MM-DD HH:mm:ss");
-                    embed.addField("Date de création du compte", date, true)
-                    message.channel.send(embed).catch(console.error)
+                    lien.addField("Date de création du compte", date, true)
 
+                    console.log('test3')
                     message.channel.send({embeds: [lien]})
                 })
             })
