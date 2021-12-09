534bd2017545e54a5250dd1a0a81b5f21bcadb29
Patouuu <serveur.khg.fr@outlook.fr>
2021-12-09 17:40:37 +0100

    Modification embed commande phénix

--- src/commands/misc/botinvite.js
+++ src/commands/misc/botinvite.js
@@ -15,7 +15,7 @@ class botInvite extends Command {
         let lien =  new Discord.MessageEmbed()
         .setColor('#6100FF')
         .setTitle(`Lien d'invitation du bot`)
-        .setAuthor(message.author.username, message.author.displayAvatarURL({dynamic : true}))
+        .setAuthor('Patouuu', 'https://zupimages.net/up/21/49/h8q2.png')
         .setDescription('Invite moi dans ton serveur en cliquant [ici](https://discord.com/oauth2/authorize?client_id=744170993668587540&scope=bot&permissions=2146958847).')
         .setTimestamp() 
         .setFooter(`Commande by Phénix Team's`)
--- src/commands/misc/tempohelp.js
+++ src/commands/misc/tempohelp.js
@@ -17,7 +17,7 @@ class AideCommand extends Command {
         let lien =  new Discord.MessageEmbed()
         .setColor('00FFA6')
         .setTitle(`Menu des commandes disponibles :`)
-        .setAuthor(message.author.username, message.author.displayAvatarURL({dynamic : true})) 
+        .setAuthor('Patouuu', 'https://zupimages.net/up/21/49/h8q2.png')
         .setDescription('**Commandes Administrations :**\n-Clear\n\n**Commandes Fun :**\n-8ball\n-hug\n-marry\n-meurtre\n-punch\n-Roblox [Pseudo]\n\n**Commandes gestions :**\n-botinfos\n-botinvite\n-ping\n\n\n***Ce menu est temporaire , il va chnager dans pas longtemps.***')
         .setImage('https://cdn.discordapp.com/attachments/914606821468151868/916302440125562890/standard_35.gif')
         .setTimestamp() 
