ac305017f30721c0e7ac315ec108f37d08b9be7f
Patouuu <serveur.khg.fr@outlook.fr>
2021-12-15 12:30:24 +0100

    modif temp help

--- src/commands/misc/tempohelp.js
+++ src/commands/misc/tempohelp.js
@@ -18,7 +18,7 @@ class AideCommand extends Command {
         .setColor('00FFA6')
         .setTitle(`Menu des commandes disponibles :`)
         .setAuthor('Patouuu', 'https://zupimages.net/up/21/49/h8q2.png')
-        .setDescription('**Commandes Administrations :**\n-Clear\n\n**Commandes Fun :**\n-8ball\n-hug\n-marry\n-meurtre\n-punch\n-Roblox [Pseudo]\n\n**Commandes gestions :**\n-botinfos\n-botinvite\n-ping\n\n**Commandes Autres :**\n-liste\n\n\n***Ce menu est temporaire , il va chnager dans pas longtemps.***')
+        .setDescription('**Commandes Administrations :**\n-Clear\n\n**Commandes Fun :**\n-8ball\n-hug\n-marry\n-meurtre\n-punch\n-Roblox [Pseudo]\n\n**Commandes gestions :**\n-botinfos\n-botinvite\n-ping\n\n**Commandes Autres :**\n-liste\n-l\n\n\n***Ce menu est temporaire , il va chnager dans pas longtemps.***')
         .setImage('https://cdn.discordapp.com/attachments/914606821468151868/916302440125562890/standard_35.gif')
         .setTimestamp() 
         .setFooter(`Commande by Phénix Team's | Tout droit reservée © 2020-2022`)
