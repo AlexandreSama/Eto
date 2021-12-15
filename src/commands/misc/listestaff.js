e92284aa631c304ce06de152b943cff00ae1fcf9
Patouuu <serveur.khg.fr@outlook.fr>
2021-12-15 11:17:29 +0100

    Maj staff

--- src/commands/misc/listestaff.js
+++ src/commands/misc/listestaff.js
@@ -19,18 +19,18 @@ class ListeStaffCommand extends Command {
         .setTitle(`Liste Des Membres Du Staff `)
         .setURL('https://discord.gg/BpPWaQq')
         .setAuthor('Patouuu', 'https://zupimages.net/up/21/49/h8q2.png')
-        .setDescription('```Ci-joint se trouvent la liste des officiel des membres de notre grande équipe PhénixMG (titre blueu = le discord.)```')
+        .setDescription('```Ci-joint se trouvent la liste officiel des membres de notre grande équipe PhénixMG (titre blueu = lien discord.)```')
         .addFields(
             { name: '💻 `Développeur Patouuu-Bot`', value: '>>> ```๖̶ζ͜͡Patoûûû༻#8321\n𝒜𝓁𝑒𝓍λ𝓃𝒹𝓇𝑒#6167```' },
             { name: '\u200B', value: '\u200B' },
-            { name: '👑 `Administrateur Discord`', value: '>>> ```๖̶ζ͜͡Patoûûû༻#8321\n𝒜𝓁𝑒𝓍λ𝓃𝒹𝓇𝑒#6167\nBillGaytte#7073```', inline: true },
-            { name: '🛡️ `Responsable Discord`', value: '>>> ```Alexander#4353``` ', inline: true },
+            { name: '👑 `Administrateur`', value: '>>> ```๖̶ζ͜͡Patoûûû༻#8321\n𝒜𝓁𝑒𝓍λ𝓃𝒹𝓇𝑒#6167\nBillGaytte#7073```', inline: true },
+            { name: '🛡️ `Responsables`', value: '>>> ```Alexander#4353\nKaier#0891\nvodka#6286\nmonsir_#5635``` ', inline: true },
             { name: '\u200B', value: '\u200B' },
-            { name: '📢 `Community Manager`', value: '>>> ```***SOON***``` ', inline: true },
-            { name: '⚔️ `Staff Discord`', value: '>>> ```cyril59310#9010\n𣝔[√σรร ησƈŧïş天#6718``` ', inline: true },
+            { name: '📢 `Community Manager`', value: '>>> ```🩸🖤 ℕ𝕃 𝔻𝕣𝕖𝕒𝕞𝕤 🖤🩸#0397\nZalphi#7088``` ', inline: true },
+            { name: '⚔️ `Modérateur Discord`', value: '>>> ```cyril59310#9010\n𣝔[√σรร ησƈŧïş天#6718``` ', inline: true },
             { name: '\u200B', value: '\u200B' },
             { name: '🚸 `Animateur(trices)`', value: '>>> ```🤍🧸💸ƇƖҽɱ💸🧸🖤#9578``` ', inline: true },
-            { name: '✍️ `Rédacteur(trices)`', value: '>>> ```***SOON***``` ', inline: true }, 
+            { name: '✍️ `Rédacteur(trices)`', value: '>>> ```***PERSONNES***``` ', inline: true }, 
             { name: '\u200B', value: '\u200B' },
             { name: '💰 `Donateur(trice)`', value: '>>> ```***PAS ENCORE DE DON***``` ', inline: true },
         )
